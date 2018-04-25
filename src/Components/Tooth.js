ad5d81e089363a98210211bcee15b218340538cb
Nico Graglia <nicograglia@gmail.com>
2018-04-25 20:47:49 -0300

    c0.0.1

--- src/Components/Tooth.js
+++ src/Components/Tooth.js
@@ -1,12 +1,34 @@
 import React, { Component } from 'react';
+import { ContextMenu, MenuItem, ContextMenuTrigger } from "react-contextmenu";
 import "./Tooth.css";
 
 const SIZE = 40;
 
 class Tooth extends Component {
+    
+    constructor(props){
+        super(props);
+
+        this.state = {
+            isActive: false,
+            Zones: {
+                center: 0,
+                top: 0,
+                bottom: 0,
+                left: 0,
+                right: 0
+            }
+        }
+    }
 
     onClick(zone, event) {
-        this.props.onClick(zone);
+        let zones = this.state.Zones;
+        zones[zone] = zones[zone] === 0 ? 1 : 2;
+        this.setState({ Zones: zones });
+    }
+
+    handleClick(e, data) {
+        console.log(data.foo);
     }
 
     render() {
@@ -21,47 +43,68 @@ class Tooth extends Component {
         var right = `${L},${0} ${b+a},${b} ${b+a},${b+a} ${L},${L}`;
         var bottom = `${0},${L} ${b},${b+a} ${b+a},${b+a} ${L},${L}`;
 
-        let getClassNamesByZone = (zone) => {
-            return "";
+        let getClassNamesByZone = (stateZones, zone) => {
+
+            let classNames = [];
+
+            if (this.state.Zones){
+                if (this.state.Zones[zone] === 1) {
+                    classNames.push('to-do');
+                } else if (this.state.Zones[zone] === 2){
+                    classNames.push('done');
+                }
+            }
+
+            return classNames.join(' ');
         }
 
         return(
             <div style={{width: `${L}px`, visibility: this.props.hidden}} className="tooth-wrapper" id={this.props.id}>
-                <svg width={L} height={L} className="tooth">
-                    <rect
-                        x={b}
-                        y={b}
-                        width={a}
-                        height={a}
-                        onClick={this.onClick.bind(this, 'center')}
-                        className={getClassNamesByZone('center')}
-                        />
+                <ContextMenu id={"ctx" + this.props.toothNumber}>
+                    <MenuItem data={{foo: 'bar'}} onClick={this.handleClick}>
+                        ContextMenu Item 1
+                    </MenuItem>
+                    <MenuItem data={{foo: 'bar2'}} onClick={this.handleClick}>
+                        ContextMenu Item 2
+                    </MenuItem>
+                </ContextMenu>
+                <ContextMenuTrigger id={"ctx" + this.props.toothNumber}>
+                    <svg width={L} height={L} className="tooth">
+                        <rect
+                            x={b}
+                            y={b}
+                            width={a}
+                            height={a}
+                            onClick={this.onClick.bind(this, 'center')}
+                            className={getClassNamesByZone(this.state.Zones, 'center')}
+                            />
 
-                    <polygon
-                        points={top}
-                        onClick={this.onClick.bind(this, 'top')}
-                        className={getClassNamesByZone('top')}
-                        />
+                        <polygon
+                            points={top}
+                            onClick={this.onClick.bind(this, 'top')}
+                            className={getClassNamesByZone(this.state.Zones, 'top')}
+                            />
 
-                    <polygon
-                        points={left}
-                        onClick={this.onClick.bind(this, 'left')}
-                        className={getClassNamesByZone('left')}
-                        />
+                        <polygon
+                            points={left}
+                            onClick={this.onClick.bind(this, 'left')}
+                            className={getClassNamesByZone(this.state.Zones, 'left')}
+                            />
 
-                    <polygon
-                        points={right}
-                        onClick={this.onClick.bind(this, 'right')}
-                        className={getClassNamesByZone('right')}
-                        />
+                        <polygon
+                            points={right}
+                            onClick={this.onClick.bind(this, 'right')}
+                            className={getClassNamesByZone(this.state.Zones, 'right')}
+                            />
 
-                    <polygon
-                        points={bottom}
-                        onClick={this.onClick.bind(this, 'bottom')}
-                        className={getClassNamesByZone('bottom')}
-                        />
+                        <polygon
+                            points={bottom}
+                            onClick={this.onClick.bind(this, 'bottom')}
+                            className={getClassNamesByZone(this.state.Zones, 'bottom')}
+                            />
 
-                </svg>
+                    </svg>
+                </ContextMenuTrigger>
                 <span style={{width: `${L}px`, display: 'block', cursor: 'pointer', margin: '0 auto', textAlign: 'center', fontSize: '10pt'}}>
                     {this.props.toothNumber}
                 </span>
@@ -71,7 +114,9 @@ class Tooth extends Component {
 }
 
 Tooth.defaultProps = {
-    toothNumber: 0
+    toothNumber: 0,
+    onClick: function() {},
+    markedZones: []
 }
 
 export default Tooth;

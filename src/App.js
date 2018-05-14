fd230c59ef12253a840c561ba02f8145a1aa1a6e
Nico Graglia <nicograglia@gmail.com>
2018-05-14 20:16:29 -0300

    onsubmit get values

--- src/App.js
+++ src/App.js
@@ -5,80 +5,90 @@ import './App.css';
 const hidd = [1, 2, 3]
 
 const left = [
-  [18,17,16,15,14,13,12,11],
-  [55,54,53,52,51],
-  [85,84,83,82,81],
-  [48,47,46,45,44,43,42,41]
+    [18, 17, 16, 15, 14, 13, 12, 11],
+    [55, 54, 53, 52, 51],
+    [85, 84, 83, 82, 81],
+    [48, 47, 46, 45, 44, 43, 42, 41]
 ]
 
 const right = [
-  [21,22,23,24,25,26,27,28],
-  [61,62,63,64,65],
-  [71,72,73,74,75],
-  [31,32,33,34,35,36,37,38]
+    [21, 22, 23, 24, 25, 26, 27, 28],
+    [61, 62, 63, 64, 65],
+    [71, 72, 73, 74, 75],
+    [31, 32, 33, 34, 35, 36, 37, 38]
 ]
 
 class App extends Component {
-  render() {
-
-    var rowCreator = (row) => {
-      return row.map((toothNumber, index) => {
-          return (
-            <Tooth key={toothNumber} toothNumber={toothNumber} />
-          )
-      })
+    submit() {
+        left.map((left_i, index) => {
+            return left_i.map((toothNumber, index) => {
+                return console.log(this.refs["tooth-" + toothNumber].state);   
+            });
+        });
     }
 
-    var hiddenToothCreator = () => {
-      return hidd.map((toothNumber, index) => {
-          return (
-            <Tooth hidden="hidden" key={`hid_${toothNumber}`}/>
-          )
-      })
-    }
+    render() {
 
-    return (
-      <div className="row odontogram">
-          <div className="col-sm-5 flex-container">
-           
-                {
-                    left.map((left_i, index) => {
-                        return (
-                          <div className="row" key={`left_row ${index}`}>
-                            <div className="col-xs-12 flex-container-right" key={`left_odontogram_row ${index}`}>
-                                {left_i.length === 5 ? hiddenToothCreator() : ""}
-                                {
-  
-                                    rowCreator(left_i)
-                                }
-                                <br/>
-                              </div>
-                            </div>
-                        );
-                    })
-                }
+        var rowCreator = (row) => {
+            return row.map((toothNumber, index) => {
+                return (
+                    <Tooth key={toothNumber} toothNumber={toothNumber} ref={"tooth-" + toothNumber} />
+                )
+            })
+        }
+
+        var hiddenToothCreator = () => {
+            return hidd.map((toothNumber, index) => {
+                return (
+                    <Tooth hidden="hidden" key={`hid_${toothNumber}`} />
+                )
+            })
+        }
+
+
+        return (
+            <div className="row odontogram">
+                <div className="col-sm-5 flex-container">
+
+                    {
+                        left.map((left_i, index) => {
+                            return (
+                                <div className="row" key={`left_row ${index}`}>
+                                    <div className="col-xs-12 flex-container-right" key={`left_odontogram_row ${index}`}>
+                                        {left_i.length === 5 ? hiddenToothCreator() : ""}
+                                        {
+
+                                            rowCreator(left_i)
+                                        }
+                                        <br />
+                                    </div>
+                                </div>
+                            );
+                        })
+                    }
+                </div>
+
+                <div className="col-sm-5 flex-container">
+
+                    {
+                        right.map((right_i, index) => {
+                            return (
+                                <div className="row" key={`right_row ${index}`}>
+                                    <div className="col-xs-12 flex-container-left" key={`right_odontogram_row ${index}`}>
+                                        {
+                                            rowCreator(right_i)
+                                        }
+                                    </div>
+                                </div>
+                            );
+                        })
+                    }
+                </div>
+
+                <button onClick={this.submit.bind(this)}>submit</button>
             </div>
-          
-          <div className="col-sm-5 flex-container">
-            
-                {
-                    right.map((right_i, index) => {
-                        return (
-                          <div className="row" key={`right_row ${index}`}>
-                            <div className="col-xs-12 flex-container-left" key={`right_odontogram_row ${index}`}>
-                                {
-                                    rowCreator(right_i)
-                                }
-                            </div>
-                            </div>
-                        );
-                    })
-                }
-            </div>
-          
-        </div>
-    );
-  }
+        );
+    }
 }
 
 export default App;
--- src/Components/Tooth.js
+++ src/Components/Tooth.js
@@ -7,15 +7,16 @@ import "./ContextMenu.css";
 const SIZE = 40;
 
 class Tooth extends Component {
-    
-    constructor(props){
+
+    constructor(props) {
         super(props);
 
         this.state = this.InitTooth();
     }
 
     InitTooth() {
-         return {
+        return {
+            toothNumber: this.props.toothNumber,
             ContextZone: "",
             Zones: {
                 center: 0,
@@ -31,38 +32,33 @@ class Tooth extends Component {
         };
     }
 
-    onClick(zone, event) {
-        // let zones = this.state.Zones;
-        // zones[zone] = 1;
-        // this.setState({ Zones: zones });
-    }
-
     handleClick(e, data, target) {
-        console.log(this.state.ContextZone);
-        if(data.Borrar === 1){
-            this.setState = this.InitTooth();
+        let toothClear = this.InitTooth();
+
+        if (data.Borrar === 1) {
+            this.setState(toothClear);
             return;
         }
 
         let zones = this.state.Zones;
 
-        if(data.CariesFull){
-            for(var property in zones) {
+        if (data.CariesFull) {
+            for (var property in zones) {
                 if (zones.hasOwnProperty(property)) {
                     zones[property] = data.CariesFull;
-                }    
+                }
             }
             this.setState({ Zones: zones });
         }
-        else if(data.Caries){
+        else if (data.Caries) {
             zones[this.state.ContextZone] = data.Caries;
             this.setState({ Zones: zones });
         } else {
-            if(data.Extract === 1 || data.Crown === 1 || data.Filter === 2 || data.Fracture === 2){
-                this.setState = this.InitTooth();
+            if (data.Extract === 1 || data.Crown === 1 || data.Filter === 2 || data.Fracture === 2) {
+                this.setState(toothClear);
             }
 
-            this.setState({ 
+            this.setState({
                 Extract: data.Extract || this.state.Extract,
                 Crown: data.Crown || this.state.Crown,
                 Filter: data.Filter || this.state.Filter,
@@ -77,24 +73,24 @@ class Tooth extends Component {
 
     render() {
         var L = SIZE;
-        var a = L/2 - L/20;
-        var b = (L - a)/2;
+        var a = L / 2 - L / 20;
+        var b = (L - a) / 2;
 
         // Template strings to define the path of the 4 poligons that define
         // 4 of 5 tooth sections
-        var top = `${0},${0} ${b},${b} ${b+a},${b} ${L},${0}`;
-        var left = `${0},${0} ${0},${L} ${b},${b+a} ${b},${b}`;
-        var right = `${L},${0} ${b+a},${b} ${b+a},${b+a} ${L},${L}`;
-        var bottom = `${0},${L} ${b},${b+a} ${b+a},${b+a} ${L},${L}`;
+        var top = `${0},${0} ${b},${b} ${b + a},${b} ${L},${0}`;
+        var left = `${0},${0} ${0},${L} ${b},${b + a} ${b},${b}`;
+        var right = `${L},${0} ${b + a},${b} ${b + a},${b + a} ${L},${L}`;
+        var bottom = `${0},${L} ${b},${b + a} ${b + a},${b + a} ${L},${L}`;
 
         let getClassNamesByZone = (stateZones, zone) => {
 
             let classNames = [];
 
-            if (this.state.Zones){
+            if (this.state.Zones) {
                 if (this.state.Zones[zone] === 1) {
                     classNames.push('to-do');
-                } else if (this.state.Zones[zone] === 2){
+                } else if (this.state.Zones[zone] === 2) {
                     classNames.push('done');
                 }
             }
@@ -102,25 +98,25 @@ class Tooth extends Component {
             return classNames.join(' ');
         }
 
-        return(
-            <div style={{width: `${L}px`, visibility: this.props.hidden}} className="tooth-wrapper" id={this.props.id}>
+        return (
+            <div style={{ width: `${L}px`, visibility: this.props.hidden }} className="tooth-wrapper" id={"tooth-" + this.props.toothNumber}>
                 <ContextMenu id={"ctx" + this.props.toothNumber}>
                     <SubMenu title='Hecho'>
-                        <MenuItem onClick={this.handleClick.bind(this)} data={{ Caries: 1}}>Caries</MenuItem>
-                        <MenuItem onClick={this.handleClick.bind(this)} data={{ CariesFull: 1}}>Caries toda la pieza</MenuItem>
-                        <MenuItem onClick={this.handleClick.bind(this)} data={{ Extract: 1}}>Ausente</MenuItem>
-                        <MenuItem onClick={this.handleClick.bind(this)} data={{ Crown: 1}}>Corona</MenuItem>
+                        <MenuItem onClick={this.handleClick.bind(this)} data={{ Caries: 1 }}>Caries</MenuItem>
+                        <MenuItem onClick={this.handleClick.bind(this)} data={{ CariesFull: 1 }}>Caries toda la pieza</MenuItem>
+                        <MenuItem onClick={this.handleClick.bind(this)} data={{ Extract: 1 }}>Ausente</MenuItem>
+                        <MenuItem onClick={this.handleClick.bind(this)} data={{ Crown: 1 }}>Corona</MenuItem>
                     </SubMenu>
                     <SubMenu title='Hacer'>
-                        <MenuItem onClick={this.handleClick.bind(this)} data={{ Caries: 2}}>Caries</MenuItem>
-                        <MenuItem onClick={this.handleClick.bind(this)} data={{ CariesFull: 2}}>Caries toda la pieza</MenuItem>
-                        <MenuItem onClick={this.handleClick.bind(this)} data={{ Extract: 2}} >Extracción</MenuItem>
-                        <MenuItem onClick={this.handleClick.bind(this)} data={{ Crown: 2}}>Corona</MenuItem>
-                        <MenuItem onClick={this.handleClick.bind(this)} data={{ Filter: 2}}>Filtrado</MenuItem>
-                        <MenuItem onClick={this.handleClick.bind(this)} data={{ Fracture: 2}}>Fractura</MenuItem>
+                        <MenuItem onClick={this.handleClick.bind(this)} data={{ Caries: 2 }}>Caries</MenuItem>
+                        <MenuItem onClick={this.handleClick.bind(this)} data={{ CariesFull: 2 }}>Caries toda la pieza</MenuItem>
+                        <MenuItem onClick={this.handleClick.bind(this)} data={{ Extract: 2 }} >Extracción</MenuItem>
+                        <MenuItem onClick={this.handleClick.bind(this)} data={{ Crown: 2 }}>Corona</MenuItem>
+                        <MenuItem onClick={this.handleClick.bind(this)} data={{ Filter: 2 }}>Filtrado</MenuItem>
+                        <MenuItem onClick={this.handleClick.bind(this)} data={{ Fracture: 2 }}>Fractura</MenuItem>
                     </SubMenu>
                     <MenuItem divider />
-                    <MenuItem onClick={this.handleClick.bind(this)} data={{ Borrar: 1}}>Borrar Todo</MenuItem>
+                    <MenuItem onClick={this.handleClick.bind(this)} data={{ Borrar: 1 }}>Borrar Todo</MenuItem>
                 </ContextMenu>
                 <ContextMenuTrigger id={"ctx" + this.props.toothNumber}>
                     <svg width={L} height={L} className="tooth">
@@ -129,71 +125,66 @@ class Tooth extends Component {
                             y={b}
                             width={a}
                             height={a}
-                            onClick={this.onClick.bind(this, 'center')}
                             className={getClassNamesByZone(this.state.Zones, 'center')}
                             onContextMenu={this.contextMyMenu.bind(this, 'center')}
-                            />
-                            
+                        />
+
                         <polygon
                             points={top}
-                            onClick={this.onClick.bind(this, 'top')}
                             className={getClassNamesByZone(this.state.Zones, 'top')}
                             onContextMenu={this.contextMyMenu.bind(this, 'top')}
-                            />
+                        />
 
                         <polygon
                             points={left}
-                            onClick={this.onClick.bind(this, 'left')}
                             className={getClassNamesByZone(this.state.Zones, 'left')}
                             onContextMenu={this.contextMyMenu.bind(this, 'left')}
-                            />
+                        />
 
                         <polygon
                             points={right}
-                            onClick={this.onClick.bind(this, 'right')}
                             className={getClassNamesByZone(this.state.Zones, 'right')}
                             onContextMenu={this.contextMyMenu.bind(this, 'right')}
-                            />
+                        />
 
                         <polygon
                             points={bottom}
-                            onClick={this.onClick.bind(this, 'bottom')}
                             className={getClassNamesByZone(this.state.Zones, 'bottom')}
                             onContextMenu={this.contextMyMenu.bind(this, 'bottom')}
-                            />
+                        />
 
-                        { this.state.Extract && this.state.Extract > 0 && 
+                        {this.state.Extract && this.state.Extract > 0 &&
                             <g stroke={this.state.Extract === 1 ? "red" : "blue"}>
                                 <line x1="0" y1="0" x2={L} y2={L} strokeWidth="3" />
                                 <line x1="0" y1={L} x2={L} y2="0" strokeWidth="3" />
                             </g>
                         }
 
-                        { this.state.Crown && this.state.Crown > 0 && 
-                            <circle 
-                                cx={L/2} 
-                                cy={L/2} r={L/2} 
-                                fill="none" 
-                                stroke={this.state.Crown === 1 ? "red" : "blue"} 
-                                strokeWidth="3"  
+                        {this.state.Crown && this.state.Crown > 0 &&
+                            <circle
+                                cx={L / 2}
+                                cy={L / 2} r={L / 2}
+                                fill="none"
+                                stroke={this.state.Crown === 1 ? "red" : "blue"}
+                                strokeWidth="3"
                             />
                         }
 
-                        { this.state.Filter && this.state.Filter > 0 && 
+                        {this.state.Filter && this.state.Filter > 0 &&
                             <g stroke={this.state.Filter === 1 ? "red" : "blue"}>
                                 <line x1="0" y1={L} x2={L} y2="0" strokeWidth="3" />
                             </g>
                         }
 
-                        { this.state.Fracture && this.state.Fracture > 0 && 
+                        {this.state.Fracture && this.state.Fracture > 0 &&
                             <g stroke={this.state.Fracture === 1 ? "red" : "blue"}>
-                                <line x1="0" y1={L/2} x2={L} y2={L/2} strokeWidth="3" />
+                                <line x1="0" y1={L / 2} x2={L} y2={L / 2} strokeWidth="3" />
                             </g>
                         }
 
                     </svg>
                 </ContextMenuTrigger>
-                <span style={{width: `${L}px`, display: 'block', cursor: 'pointer', margin: '0 auto', textAlign: 'center', fontSize: '10pt'}}>
+                <span style={{ width: `${L}px`, display: 'block', cursor: 'pointer', margin: '0 auto', textAlign: 'center', fontSize: '10pt' }}>
                     {this.props.toothNumber}
                 </span>
             </div>
@@ -203,7 +194,7 @@ class Tooth extends Component {
 
 Tooth.defaultProps = {
     toothNumber: 0,
-    onClick: function() {},
+    onClick: function () { },
     markedZones: []
 }
 
